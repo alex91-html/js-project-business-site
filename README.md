9d0ec7d1fef7af561273a234973ff41d85a84ee6
alex91-html <alex91-html@users.noreply.github.com>
2025-02-22 11:05:23 +0100

    last required updates

--- README.md
+++ README.md
@@ -6,8 +6,6 @@ netlify link: https://designshop.netlify.app/
 
 <h3>STILL TO DO:</h3>
 <ul>
-<li>fixing css and style of the hero video</li>
 <li>fixing a new logo</li>
-<li>!! adding JavaScript !! </il>
 </ul>
 
--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// console.log('Script loaded');    
+const scrollButton = document.getElementById('scrollButton');
+const targetSection = document.getElementById('targetSection');
+const burgerMenu = document.querySelector('.burger-menu');
+const navList = document.querySelector('.nav-list1');
+
+// click on button – sign up – scrolls to bottom of page 
+scrollButton.addEventListener('click', () => {
+  targetSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+// click on burger menu - show navigation list
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.toggle('active');
+  navList.classList.toggle('active');
+});
